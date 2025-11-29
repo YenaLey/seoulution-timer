a43a0d99ed18672ee1fd9f2d8f487b77b5dc5549
YenaLey <yena.e121@gmail.com>
2025-11-29 09:04:54 +0900

    chore: config.js 수정

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
-  TARGET_DATE: new Date("2025-11-29T09:00:00"),
+  TARGET_DATE: new Date("2025-11-29T10:40:00"),
   // TARGET_DATE: new Date("2025-11-29T10:40:00"),
-  TITLE: "개발 마감까지",
+  TITLE: "발표 자료 제출 마감까지",
   TEXT_COLOR: "#ffffff",
   BACKGROUND_COLOR: "#000000",
 };
